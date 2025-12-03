1a5b91e33d9e155bdbfcdbf7ede83ffa80c2f5f8
giobros <giorgia.brosio@iit.it>
2025-12-03 12:48:46 -0500

    Update run.sh

--- container/run.sh
+++ container/run.sh
@@ -1,22 +1,36 @@
 #!/bin/bash
+set -euo pipefail
 
 unset PYTHONPATH
+
+# ---------------- USER SETTINGS ----------------
+
+ROOT="ROOT"                     # CHANGE THIS! name of your PDB, without .pdb
+DATA_DIR="$HOME/your_folder"    # CHANGE THIS! folder that contains $ROOT.pdb
+
+METHOD="all"                     # "all" | "mace" | "obi"
+DIHEDRAL="all"                   # "all" | "[a,b,c,d]" | "print"  (0-based indices)
+CONF_ANALYSIS="false"            # "true" | "false" | "none"
+BCS="false"                      # "true" | "false" | "none"
+MCS="false"                      # "true" | "false" | "none"
+N_CONF=50                        # number of conformers
+RMSD=0.5                         # RMSD pruning threshold
+
+# ------------------------------------------------
 
 apptainer exec \
   --nv \
   --bind "$HOME/torsionator/torsionator:/torsionator" \
-  --bind "$HOME/<your_folder>:/data" \     ## MODIFY <your_folder> WITH YOU ACTUAL FOLDER NAME
+  --bind "$DATA_DIR:/data" \
   --env PYTHONPATH=/ \
   --env HOME=/root \
   torsionator.sif \
   python3.9 -m torsionator.cli \
-   --pdb /data/<ROOT>.pdb \   # MODIFY <ROOT> WITH YOU ACTUAL PDB NAME
-   --method obi|mace|all   #default=all   
-   --dihedral all|[a,b,c,d]|"print"  #default=all \   # INDICES 0-BASED \
-   --conf_analysis true|false|none #default=false \
-   --BCS true|false|none  # Best-Conformer-Scan default=false\
-   --MCS true|false|none # Multi-Conformer-Scan  default=false \
-   --N # N. of conformers to generate #default=50 \
-   --RMSD # RMSD pruning threshold  #default=0.5 \
-
-
+    --pdb "/data/${ROOT}.pdb" \
+    --method "$METHOD" \
+    --dihedral "$DIHEDRAL" \
+    --conf_analysis "$CONF_ANALYSIS" \
+    --BCS "$BCS" \
+    --MCS "$MCS" \
+    --N "$N_CONF" \
+    --RMSD "$RMSD"
