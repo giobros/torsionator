4dd135b1c849eb53d5389c9cdea851b0a12b7a55
giobros <giorgia.brosio@iit.it>
2025-12-03 12:40:47 -0500

    Update run.sh

--- container/run.sh
+++ container/run.sh
@@ -10,13 +10,13 @@ apptainer exec \
   --env HOME=/root \
   torsionator.sif \
   python3.9 -m torsionator.cli \
-   --pdb /data/<ROOT>.pdb \   ## MODIFY <ROOT> WITH YOU ACTUAL PDB NAME
-   --method obi|mace|all \      
-   --dihedral all|[a,b,c,d]|"print" \   # INDICES 0-BASED
-   --conf_analysis true|false \
-   --BCS true|false # Best-Conformer-Scan
-   --MCS true|false # Multi-Conformer-Scan
-   --N # N. of conformers to generate
-   --RMSD # RMSD pruning threshold
+   --pdb /data/<ROOT>.pdb \   # MODIFY <ROOT> WITH YOU ACTUAL PDB NAME
+   --method obi|mace|all   #default=all   
+   --dihedral all|[a,b,c,d]|"print"  #default=all \   # INDICES 0-BASED \
+   --conf_analysis true|false|none #default=false \
+   --BCS true|false|none  # Best-Conformer-Scan default=false\
+   --MCS true|false|none # Multi-Conformer-Scan  default=false \
+   --N # N. of conformers to generate #default=50 \
+   --RMSD # RMSD pruning threshold  #default=0.5 \
 
 
